50a5b43637dbf306eab7697f0e063a0bebec2181
Alex <alexwerner9@gmail.com>
2021-07-14 20:09:38 -0700

    Binary tree update

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,7 @@ public class Main {
   public static void main(String[] args) {
 
     Dynamic_Array arrayList = new Dynamic_Array(3);
-    Binary_Tree binaryTree = new Binary_Tree();
-    arrayList.insert(5);
-    arrayList.insert(6);
-    arrayList.insert(7);
-    arrayList.insert(8);
-    arrayList.insert(9);
-    
-    System.out.println(arrayList.locationOf(6));
-    System.out.println(arrayList.toString());
+    Binary_Tree binaryTree = new Binary_Tree(4);
 
   }
 
--- src/binary_tree/Binary_Tree.java
+++ src/binary_tree/Binary_Tree.java
@@ -1,17 +1,18 @@
 package binary_tree;
 
 import dynamic_array.Dynamic_Array;
+
+import java.util.Arrays;
 import java.util.Random;
 
 public class Binary_Tree {
 
   Tree_Node rootNode;
 	
-  public Binary_Tree() {
+  public Binary_Tree(int height) {
 
     rootNode = new Tree_Node(-1);
-    
-    initRandomTree(4);
+    initRandomTree(height);
 
   }
 
@@ -19,22 +20,24 @@ public class Binary_Tree {
 
     Random rand = new Random();
     int counter = 0;
-    Tree_Node[] prevLeaves = new Tree_Node[2];
+    Tree_Node[] prevLeaves = new Tree_Node[1];
     Tree_Node[] newLeaves = new Tree_Node[2];
+    
+    prevLeaves[0] = rootNode;
 
-    for(int n = 1; n < height; n++) {
+    for(int level = 1; level < height; level++) {
 
-      newLeaves = new Tree_Node[(int)Math.pow(2, n)];
+      newLeaves = new Tree_Node[(int)Math.pow(2, level)];
     	
       for(int i = 0; i < prevLeaves.length; i++) {
 
-        newLeaves[i] = addNode(rootNode, "left", rand.nextInt(100));
-        newLeaves[i] = addNode(rootNode, "right", rand.nextInt(100));
+        newLeaves[i * 2] = addNode(prevLeaves[i], "left", rand.nextInt(100));
+        newLeaves[i * 2 + 1] = addNode(prevLeaves[i], "right", rand.nextInt(100));
         counter++;
 
       } prevLeaves = newLeaves;
-
-    } System.out.println(counter);
+      
+    }
 
   }
 
